891d65606e70a05576a1b33c24e00eba97853754
System Administrator <root@MacBook-Pro-de-Nathan.local>
2018-11-16 14:36:46 +0100

    commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+
+    "rules": {
+        "header-max-length": [2, "always", 72],
+        "type-enum": [2, "always", ['feat', 'fix', 'docs', 'style', 'style', 'refactor', 'test', 'revert', 'chore']]
+    }
