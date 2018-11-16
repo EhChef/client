3593e89ebeee845050ca4e100b47dc83e9fed174
System Administrator <root@MacBook-Pro-de-Nathan.local>
2018-11-16 15:04:10 +0100

    chore: set commit lint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
 
-    "rules": {
+    rules: {
         "header-max-length": [2, "always", 72],
-        "type-enum": [2, "always", ['feat', 'fix', 'docs', 'style', 'style', 'refactor', 'test', 'revert', 'chore']]
+        "type-enum": [2, "always", ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'chore']]
     }
+}
